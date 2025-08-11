b52df6df8eb9ad6fd125015c53975a740e24fcf6
ShahabuddinAnsari <ansari.extc@gmail.com>
2025-08-11 17:38:22 +0530

    Updated

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,18 @@
-sap.ui.define(["sap/m/Text"], (Text) => {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], (XMLView) => {
   "use strict";
 
-  new Text({
-    text: "Hello, SAP UI5 Walkthrough!",
-    id: "helloText",
-  }).placeAt("content");
+  // new Text({
+  //   text: "Hello, SAP UI5 Walkthrough!",
+  //   id: "helloText",
+  // }).placeAt("content");
+
+  XMLView.create({
+    viewName: "ui5.walkthrough.view.App",
+  })
+    .then((oView) => {
+      oView.placeAt("content");
+    })
+    .catch((error) => {
+      console.error("Error creating view:", error);
+    });
 });
