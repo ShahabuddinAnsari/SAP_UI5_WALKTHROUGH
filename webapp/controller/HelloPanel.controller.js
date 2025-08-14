e8e404858baef979a249934f555cccc22571db80
Shahabuddin Mohammed Bannu Ansari <ansari.extc@gmail.com>
2025-08-14 10:43:28 +0000

    Added icons

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,15 +1,33 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function(Controller, MessageToast) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
     "use strict";
 
-    return Controller.extend("sap.ui5.walkthrough.controller.HelloPanel", {
-        onShowHello() {
-         // read msg from i18n model
-         const oBundle = this.getView().getModel("i18n").getResourceBundle();
-         const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+    return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
+      onSayHelloButtonPress() {
+        // read msg from i18n model
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-         // show message
-         MessageToast.show(sMsg);
+        // show message
+        MessageToast.show(sMsg);
+      },
+
+      async onOpenDialog() {
+        // create dialog lazily
+        this.oDialog ??= await this.loadFragment({
+          name: "ui5.walkthrough.view.HelloDialog",
+        });
+      
+        this.oDialog.open();
+      },
+
+      onCloseDialog() {
+        this.byId("helloDialog").close();
       }
     });
-});
+  }
+);
